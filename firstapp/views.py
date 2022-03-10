938ce84da03dbd4feb4a8ee025eb911673b60f20
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-03-10 07:38:39 +0200

    feat:add order to admin.py  in fistapp

--- firstapp/views.py
+++ firstapp/views.py
@@ -3,6 +3,9 @@ from firstapp.models import Pizza
 
 from firstapp.forms import OrderForm
 
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 # Create your views here.
 def home(request):
     pizzas = Pizza.objects.all()
@@ -10,10 +13,17 @@ def home(request):
 
 def pizza_detail(request, pizza_id):
     pizza = get_object_or_404(Pizza, id=pizza_id)
-    form = OrderForm(initial={
+    form = OrderForm(request.POST or None, initial={
         'pizza': pizza
     })
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('{}?sent=True'.format(reverse('pizza-detail', kwargs={'pizza_id': pizza.id})))
+
     return render(request, 'pizza_detail.html', {
         'pizza': pizza,
-        'form': form
+        'form': form,
+        'sent': request.GET.get('sent', False)
     }) 
